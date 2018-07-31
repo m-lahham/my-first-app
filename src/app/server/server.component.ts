ddb37c02d72566316b4609088b00918dc55440ae
Mohammad Al Lahham <Lahham@sts.com.jo>
2018-07-31 14:20:00 +0300

    Practice Until: The Basics (Section 2) Lecture 38

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,14 +4,26 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'app-server',
-    templateUrl : './server.component.html'
-   
+    templateUrl : './server.component.html',
+    styles : [`
+        .online {
+            color : white
+        }`,`
+        .on-border{
+            border: 1px solid #eaff00 !important ;
+            padding: 0.01em 16px;
+        }`
+    ]
 
 })
 export class ServerComponent{
 
-    serverStatus:String = 'Offline';
+    serverStatus:String = Math.random() > 0.5 ?'Offline' : 'Online';
     serverId:number = 10;
+
+    getColor(){
+        return this.serverStatus == 'Online' ?'green':'red';
+    }
 
     getServerStatus(){
         return this.serverStatus;
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,9 @@ import { timeout } from 'q';
 export class ServersComponent implements OnInit {
   allowAddServer = false;
   serverAddServerStatus ='No new server added';
-  serverNameInput:String;
+  serverNameInput:string;
+  isServerCreated = false;
+  servers = ['Test Server 1', 'Test Server 2'];
   constructor() { 
     setTimeout(()=>{this.allowAddServer = true;},2000);
 
@@ -20,6 +22,8 @@ export class ServersComponent implements OnInit {
   }
 
   onAddNewServer(){
+    this.isServerCreated = true;
+    this.servers.push(this.serverNameInput);
     this.serverAddServerStatus = 'New server has been added';
   }
 
